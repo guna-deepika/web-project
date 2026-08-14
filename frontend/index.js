5e63d164bbcd2fd26c3ad0914a9b95fad151d12f
guna <gunadeepika33@gmail.com>
2026-08-14 11:10:08 +0530

    color

--- frontend/index.js
+++ frontend/index.js
@@ -24,3 +24,23 @@ function toggle(){
  document.getElementById("card-name").innerText =users[curIndex].name
  //gender needs to be updated
 }
+
+function random(){
+    fetch('https://randomuser.me/api')
+        .then(function(response){
+            return response.json();
+
+        })
+        .then(function(data){
+            var detail=data.results[0];
+            var fullname=detail.name.title+" "+detail.name.first+" "+detail.name.last
+            
+            document.getElementById("card-image").src=detail.picture.large
+
+            
+            document.getElementById("card-gender").innerText=detail.gender
+            document.getElementById("card-name").innerText=fullname
+            
+        })
+
+}
